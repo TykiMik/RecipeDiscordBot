b8f7190296c200fdea19db819dfa918d5fef7911
Simon Csongor <simon.csongor2@gmail.com>
2021-11-15 22:40:19 +0100

    flask RestApi starter

--- Backend/flaskapp/app.py
+++ Backend/flaskapp/app.py
@@ -1,23 +1,30 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_restful import Resource, Api
 from flask_pymongo import PyMongo
+from flaskapp.models.RecipeModel import RecipeModel
 
 application = Flask(__name__)
-
-application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ[
-    'MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
+api = Api(application)
+application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + \
+            os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
 
 mongo = PyMongo(application)
 db = mongo.db
+recipe_collection = db['Recipes']
 
 
-@application.route('/')
-def index():
-    application.logger.info("GOT A REQUEST!")
-    return jsonify(
-        status=True,
-        message='Welcome to the...whatever this is!'
-    )
+class Recipes(Resource):
+    def get(self):
+        recipes = []
+        for recipe in recipe_collection.find({}):
+            recipes.append(RecipeModel(**recipe))
+        return {
+            "recipes": [recipe.to_json() for recipe in recipes],
+        }
+
+
+api.add_resource(Recipes, '/', '/Recipes')
 
 
 if __name__ == "__main__":
