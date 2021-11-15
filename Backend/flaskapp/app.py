0bd45d4328756507e5dc4bdcedd713f2548ecafa
Németh Krisztián <nemethk@ukatemi.com>
2021-11-15 19:59:05 +0100

    Added dockerized backend

---
+++ Backend/flaskapp/app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+
+application = Flask(__name__)
+
+application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ[
+    'MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
+
+mongo = PyMongo(application)
+db = mongo.db
+
+
+@application.route('/')
+def index():
+    application.logger.info("GOT A REQUEST!")
+    return jsonify(
+        status=True,
+        message='Welcome to the...whatever this is!'
+    )
+
+
+if __name__ == "__main__":
+    ENV_DEBUG = os.environ.get("APP_DEBUG", True)
+    ENV_PORT = os.environ.get("APP_PORT", 5000)
+
+    application.run(host='0.0.0.0', port=ENV_PORT, debug=ENV_DEBUG)
