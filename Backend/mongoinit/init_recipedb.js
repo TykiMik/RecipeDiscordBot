d07fe10d0d42b6a74522773a0f805df758d668de
Németh Krisztián <nemethk@ukatemi.com>
2021-11-23 12:43:22 +0100

    Fixed faulty recipe.

--- Backend/mongoinit/init_recipedb.js
+++ Backend/mongoinit/init_recipedb.js
@@ -19,7 +19,8 @@ recipes = [
         "content": "2 pounds baking potatoes, peeled and quartered\n2 tablespoons butter\n1 cup milk\nsalt and pepper to taste\nBring a pot of salted water to a boil. Add potatoes and cook until tender but still firm, about 15 minutes; drain.\nIn a small saucepan heat butter and milk over low heat until butter is melted. Using a potato masher or electric beater, slowly blend milk mixture into potatoes until smooth and creamy. Season with salt and pepper to taste.",
         "tags": ["potato", "mash", "side"],
         "request_count": 415,
-        "ratings": [5,4,4,3,5,4,1,5,2,4,5]
+        "ratings": [5,4,4,3,5,4,1,5,2,4,5],
+        "creation_date": new Date(2021, 11, 19, 5, 41, 14, 4)
     }
 ];
 
